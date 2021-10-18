ac2a8744a58f50af28479528fbc04df54bb70a75
Daniel Szarszewski <daniel.szarszewski@wp.pl>
2021-10-18 19:45:09 +0200

    V.1.1
    Działająca aplikacja, pora na stylistyke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
+    state={
+      text: " ",
+      error: "Error: Not Found"
+    }
+
+    handleDateChange = (e) => {
+      const value = this.refs.number.value;
+      console.log(value);
+      fetch(`http://numbersapi.com/${value}/year?json`)
+      //Respons
+      .then(res=>res.json())
+      .then(data=>this.setState({
+        text:data.text
+      })) //setState służy do zmiany stanów el. reactowych
+      //.then(data=>console.log(data.text)) wyswietla wynik w konsoli
+      //http://numbersapi.com/{value}/year?json
+      .catch(err=> console.log(err))
+    }
+
+    render() {
+      return(
+        <div>Enter the year:<input onChange={this.handleDateChange} type="text" ref="number"/>
+         <p>Answer:{this.state.text}</p>
+        
+        </div>
+       
+        
+      );
+    }
+}
 export default App;
